359a236be114c42be43fb8faad9786af576e0476
Elmozll <77222864+Elmozll@users.noreply.github.com>
2022-05-03 03:10:53 +0800

    new file user.java

---
+++ src/main/java/com/elmo/pojo/User.java
@@ -0,0 +1,11 @@
+package com.elmo.pojo;
+
+/**
+ * @author Elmo Zhou
+ * @version 1.0
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
